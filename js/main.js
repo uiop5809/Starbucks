6d9835e8ff355091027f46e1b0f71139a68e9038
parkyejin <uiop5809@naver.com>
2022-02-22 15:44:49 +0900

    22.02.22

--- js/main.js
+++ js/main.js
@@ -20,7 +20,6 @@ const badgeEl = document.querySelector("header .badges");
 window.addEventListener(
   "scroll",
   _.throttle(function () {
-    console.log(window.scrollY);
     if (window.scrollY > 500) {
       gsap.to(badgeEl, 0.6, {
         //배지 숨기기
@@ -38,10 +37,51 @@ window.addEventListener(
 ); //_.throttle(함수, 시간)
 
 const fadeEls = document.querySelectorAll(".visual .fade-in");
+
 fadeEls.forEach(function (fadeEl, index) {
   //gsap.to(요소, 지속시간, {옵션});
   gsap.to(fadeEl, 1, {
     delay: (index + 1) * 0.7,
     opacity: 1,
   });
+});
+
+//new Swiper(선택자, 옵션)
+new Swiper(".notice-line .swiper-container", {
+  direction: "vertical",
+  autoplay: true,
+  loop: true,
+});
+new Swiper(".promotion .swiper-container", {
+  slidesPerView: 3, //한 번에 보여줄 슬라이드 개수
+  spaceBetween: 10, //슬라이드 사이 여백
+  centeredSlides: true, //1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: ".promotion .swiper-pagination", //페이지 번호 요소 선택자
+    clickable: true, //사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
+});
+
+const promotionEl = document.querySelector(".promotion");
+const promotionToggleBtn = document.querySelector(".toggle-promotion");
+
+let isHidePromotion = false;
+
+promotionToggleBtn.addEventListener("click", function () {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    //숨김 처리
+    promotionEl.classList.add("hide");
+  } else {
+    //보임 처리
+    promotionEl.classList.remove("hide");
+  }
 });
